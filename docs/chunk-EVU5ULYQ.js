35f887779fb07d398212c08974a98bbad6ff2075
Marco <marcomichael@t-online.de>
2024-09-04 13:26:18 +0200

    fix url

---
+++ docs/chunk-EVU5ULYQ.js
@@ -0,0 +1,1 @@
+var e={production:!0,userApiUrl:"https://slg96-service-75920487391.europe-west1.run.app/api/users",questionApiUrl:"https://slg96-service-75920487391.europe-west1.run.app/api/question",calendarApiUrl:"https://slg96-service-75920487391.europe-west1.run.app/api/calendar",authApiUrl:"https://slg96-service-75920487391.europe-west1.run.app/api/authenticate"};export{e as a};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
     userApiUrl: 'https://slg96-service-75920487391.europe-west1.run.app/api/users',
     questionApiUrl: 'https://slg96-service-75920487391.europe-west1.run.app/api/question',
     calendarApiUrl: 'https://slg96-service-75920487391.europe-west1.run.app/api/calendar',
-    authApiUrl: 'https://slg96-service-75920487391.europe-west1.run.app/authenticate',
+    authApiUrl: 'https://slg96-service-75920487391.europe-west1.run.app/api/authenticate',   
   };
   
